82a993977dd3e9acf61f1ffb5d8584026db24931
Misenpai <misenpai200231@gmail.com>
2023-10-26 10:13:15 +0530

    Step-6

--- lib/pages/home/widgit/current.dart
+++ lib/pages/home/widgit/current.dart
@@ -1,7 +1,15 @@
+import 'package:fitness_tracker/models/fitness_program.dart';
 import 'package:flutter/material.dart';
 
-class CurrentPrograms extends StatelessWidget {
+class CurrentPrograms extends StatefulWidget {
   const CurrentPrograms({super.key});
+
+  @override
+  State<CurrentPrograms> createState() => _CurrentProgramsState();
+}
+
+class _CurrentProgramsState extends State<CurrentPrograms> {
+  ProgramType active = fitnessPrograms[0].type;
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +37,19 @@ class CurrentPrograms extends StatelessWidget {
         SizedBox(
           width: double.infinity,
           height: 100,
-          child: ListView.builder(
+          child: ListView.separated(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
             scrollDirection: Axis.horizontal,
+            itemCount: fitnessPrograms.length,
             itemBuilder: (context, index) {
-              return Program();
+              return Program(
+                program: fitnessPrograms[index],
+                active: fitnessPrograms[index].type == active,
+              );
             },
+            separatorBuilder: (context, index) => SizedBox(
+              width: 20,
+            ),
           ),
         )
       ],
@@ -42,13 +58,60 @@ class CurrentPrograms extends StatelessWidget {
 }
 
 class Program extends StatelessWidget {
-  const Program({super.key});
+  final FitnessProgram program;
+  final bool active;
+
+  const Program({super.key, required this.program, this.active = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
       width: 180,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+              active
+                  ? Color(0xff1ebdf8).withOpacity(.8)
+                  : Colors.white.withOpacity(.8),
+              BlendMode.lighten,
+            ),
+            image: program.image,
+            fit: BoxFit.cover,
+          )),
+      alignment: Alignment.bottomLeft,
+      padding: const EdgeInsets.all(15),
+      child: DefaultTextStyle.merge(
+        style: TextStyle(
+          color: active ? Colors.white : Colors.black,
+          fontSize: 10,
+          fontWeight: FontWeight.w500,
+        ),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(program.name),
+              Row(
+                children: [
+                  Text(program.cals),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Icon(
+                    Icons.timer,
+                    size: 10,
+                    color: active ? Colors.white : Colors.black,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(program.time),
+                ],
+              )
+            ]),
+      ),
     );
   }
 }
